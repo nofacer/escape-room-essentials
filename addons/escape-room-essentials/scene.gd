34b5a7754c066746802d9713013e3648d70fc1a3
nofacer <nofacer@outlook.com>
2024-06-20 23:44:16 +0800

    update

--- addons/escape-room-essentials/scene.gd
+++ addons/escape-room-essentials/scene.gd
@@ -1,3 +1,31 @@
 @icon("res://addons/escape-room-essentials/scene.svg")
 class_name Scene
 extends Node
+
+@onready var scene_nav_list = get_tree().get_nodes_in_group("secne_nav")
+@onready var scene_trans_list: Array[SceneTrans] = (
+	get_tree().get_nodes_in_group("secne_trans") as Array[SceneTrans]
+)
+@onready var scene_trnas_num = scene_trans_list.size()
+var completed_trans := 0
+
+
+func _ready() -> void:
+	for scene_nav in scene_nav_list:
+		scene_nav.connect("nav_to", _on_nav_to)
+	for scene_trans in scene_trans_list:
+		scene_trans.enter()
+
+
+func _on_nav_to(scene: String):
+	if scene_trnas_num == 0:
+		get_tree().change_scene_to_file(scene)
+	for scene_trans in scene_trans_list:
+		scene_trans.connect("leave_complete", _on_leave_complete.bind(scene))
+		scene_trans.leave()
+
+
+func _on_leave_complete(scene: String):
+	completed_trans += 1
+	if completed_trans == scene_trnas_num:
+		get_tree().change_scene_to_file(scene)
--- addons/escape-room-essentials/scene_nav.gd
+++ addons/escape-room-essentials/scene_nav.gd
@@ -1,3 +1,17 @@
 @icon("res://addons/escape-room-essentials/scene_nav.svg")
 class_name SceneNav
 extends Area2D
+
+signal nav_to(target_scene: String)
+
+@export_file var target_scene
+
+
+func _ready() -> void:
+	add_to_group("scene_nav")
+
+
+func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
+	if not event.is_action_pressed("interact"):
+		return
+	nav_to.emit(target_scene)
